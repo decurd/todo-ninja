338855013bf4592c7a7719101aff88ad4ba502fd
decurd <coffee5158@naver.com>
2018-12-14 08:41:48 +0900

    ..

--- src/views/PaddingnMargin.vue
+++ src/views/PaddingnMargin.vue
@@ -2,7 +2,7 @@
   <div class="paddingnmargin">
     <h1 class="subheading grey--text">This is an Padding & Margin</h1>
 
-    <v-container class="blue my-5">
+    <v-container class="blue my-5 white--text">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ipsam ex pariatur explicabo libero accusantium magnam dignissimos nam? Explicabo commodi, inventore iure dolores ea culpa iste cupiditate id soluta nemo?</p> 
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim omnis odio autem nihil iure quos blanditiis exercitationem ea porro id rem, ut fugit ullam nulla? In vitae eligendi consectetur earum.</p>
     </v-container>
