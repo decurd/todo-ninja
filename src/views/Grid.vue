1e642bf246069b7ebc1b561e4d9f554880dcbfde
decurd <coffee5158@naver.com>
2018-12-14 17:34:49 +0900

    ..

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -19,5 +19,28 @@
             
         </v-layout>
     </v-container>
+
+    <v-container class="my-5">
+        <v-card flat class="pa-3">
+            <v-layout row wrap>
+                <v-flex xs12 md6>
+                    <div class="caption grey--text">Project Title</div>
+                    <div>Create a new Website</div>
+                </v-flex>
+                <v-flex xs6 sm4 md2>
+                    <div class="caption grey--text">Person</div>
+                    <div>The Net Ninja</div>
+                </v-flex>
+                <v-flex xs6 sm4 md2>
+                    <div class="caption grey--text">Due by</div>
+                    <div>1st Jan 2019</div>
+                </v-flex>
+                <v-flex xs2 sm4 md2>
+                    <div class="caption grey--text">Status</div>
+                    <div>ongoing</div>
+                </v-flex>
+            </v-layout>
+        </v-card>
+    </v-container>
   </div>
 </template>
